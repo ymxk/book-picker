5a0805a7ae7fb2487672ac7f4634d0c78e732e86
ymxk <ymxk666@163.com>
2019-06-18 16:04:10 +0800

    4 files modified, 7 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,13 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { TimePickerComponent } from './time-picker/time-picker.component';
+import { DatePickerComponent } from './date-picker/date-picker.component';
+import { BookInfoComponent } from './book-info/book-info.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, HelloComponent, TimePickerComponent, DatePickerComponent, BookInfoComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ src/app/date-picker/date-picker.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import moment from 'moment';
+
+@Component({
+  selector: 'app-date-picker',
+  templateUrl: './date-picker.component.html',
+  styleUrls: ['./date-picker.component.css']
+})
+export class DatePickerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
