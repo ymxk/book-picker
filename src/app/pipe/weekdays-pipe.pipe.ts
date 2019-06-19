f67c3683770a03f651571e80f8b6b6956438cf13
ymxk <ymxk666@163.com>
2019-06-19 14:24:39 +0800

    1 file modified

--- src/app/pipe/weekdays-pipe.pipe.ts
+++ src/app/pipe/weekdays-pipe.pipe.ts
@@ -1,18 +1,18 @@
 import { Pipe, PipeTransform } from '@angular/core';
-import * as moment from 'moment';
+import moment from 'moment';
 
 @Pipe({
   name: 'weekdays'
 })
 export class WeekdaysPipePipe implements PipeTransform {
-
+  now: any = moment();
   weekdays: string[] = [
     "天", "一", "二", "三", "四", "五", "六"
   ]
 
   transform(value: moment.Moment): string {
     if (!value) { return ''; }
-    return this.weekdays[value.day()];
+    return this.now.isSame(value, 'day') ? '今日' : this.weekdays[value.day()];
   }
 
 }
