4d80fbb121378496300416637999d0628d9ea8e4
ymxk <ymxk666@163.com>
2019-06-19 18:27:25 +0800

    1 file modified

--- src/app/time-picker/time-picker.component.ts
+++ src/app/time-picker/time-picker.component.ts
@@ -58,7 +58,7 @@ export class TimePickerComponent implements OnInit {
     this.hours = [];
     let start;
     if (moment().isSame(this.nowTime, 'day')) {
-      console.log("now ");
+      console.log("now  ");
       const y = parseFloat(moment().format('mm')) % 30;
       start = moment().subtract(y, 'm').add(30, 'm');
     } else {
