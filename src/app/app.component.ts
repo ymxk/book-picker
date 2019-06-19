b070a98a13126ba79a63444535953b63eadc49ca
ymxk <ymxk666@163.com>
2019-06-19 17:29:05 +0800

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,12 +4,13 @@ import moment from 'moment';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent  {
+export class AppComponent {
   name = 'Book Picker';
+  selectedDate: moment.Moment;
 
-  onSelectedDate(value: moment.Moment){
-    console.log('onSelectedDate', value);
+  onSelectedDate(value: moment.Moment) {
+    this.selectedDate = value;
   }
 }
